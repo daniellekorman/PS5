8ae383090f20c35f602ca7d28e5cc867e7049033
Danielle Korman <d.korman@wustl.edu>
2016-03-10 07:50:29 -0600

    Models
    Working on linear regression models

--- PS5.R
+++ PS5.R
@@ -35,3 +35,41 @@ test <- anes[indexes,]
 dim(test) 
 train <- anes[-indexes,]
 dim(train)
+
+# statistical models
+# First Model: Regress Obama Thermometer on Gender
+# Use variable gender_respondent_x for gender, as this has the gender for both
+# FTF and web modes
+# 1 is male and 2 is female
+# There does not appear to be any missing observations
+str(anes$gender_respondent_x)
+length(anes$gender_respondent_x)
+class(anes$gender_respondent_x)
+unique(anes$gender_respondent_x)
+# It is currently a factor, so it must be made numeric
+gender_num <- as.numeric(train$gender_respondent_x)
+# Regress against Obama Thermometer variable
+gen_model <- lm(ft_dpc ~ gender_num, train)
+
+# Second Model:  Obama Thermometer on education
+# use dem_edu, which has 18 unique outcomes
+# While the codebook doesn't specify every outcome, it can be assumed from
+# the information they do give that a higher value is more education
+# change from factor to nuumeric
+edu_num <- as.numeric(train$dem_edu)
+# look at unique values: they are numbers 1-19, but there is no 2
+table(edu_num)
+# regress Obama thermometer on education
+edu_model <- lm(ft_dpc ~ edu_num, train)
+
+# Third model: Obama thermometer on vice president thermometer (Biden)
+table(train$ft_dvpc)
+# Has the same possible values as 
+
+
+
+# among women
+predict(gen_model, data.frame(gender_num=2))
+# among people with bachelors degrees
+predict(edu_model, data.frame(edu_num=13))
+
