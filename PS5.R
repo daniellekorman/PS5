cc010348c92c472bd525c5576bbac67c5f665e54
Danielle Korman <d.korman@wustl.edu>
2016-03-10 10:31:42 -0600

    Models completed
    went back and made data easier to work with, completed 3 regressions

--- PS5.R
+++ PS5.R
@@ -2,7 +2,7 @@
 ## March 10
 ## Danielle Korman
 
-# Code given:
+# Code given to set up:
 
 rm(list=ls())
 set.seed(12435)
@@ -28,41 +28,59 @@ predict(model1, data.frame(ft_hclinton=77))
 ## of Obama's feeling thermometer score
 ## document carefully how you deal with missingness
 
+# First get rid of negative numbers in Obama thermometer variable, ft_dpc
+table(anes$ft_dpc)
+# change to NA
+anes$ft_dpc[anes$ft_dpc==-9] <- NA
+anes$ft_dpc[anes$ft_dpc==-8] <- NA
+anes$ft_dpc[anes$ft_dpc==-2] <- NA
+
+# Make table of variables using for regression
+anes2 <- subset(anes, select=c("ft_dpc", "gender_respondent_x", "dem_edu", 
+                               "ft_rpc", "ft_gwb", "ft_rep"))
+colnames(anes2) <- c("Obama", "Gender", "Education", "Romney", "Bush", "Rep")
+
+# Make all data that does not fit linearly NA
+table(anes2$Gender)
+anes2$Gender <- as.numeric(anes2$Gender)
+# Gender has no NA and only 1's and 2's
+table(as.numeric(anes2$Education))
+anes2$Education <- as.numeric(anes2$Education)
+# Eduction needs 1 and 19 to be NA because in the numeric form
+# these are the same as -9 and 95
+anes2$Education[anes2$Education==1] <- NA
+anes2$Education[anes2$Education==19] <- NA
+table(anes2$Education)
+# The last 3 variables are also thermometers and will be the same as Obama's
+anes2[anes2==-8] <- NA
+anes2[anes2==-9] <- NA
+anes2[anes2==-2] <- NA
+
 # Split data into test and train
-dim(anes)
-indexes <- sample(1:nrow(anes), size=0.5*nrow(anes))
-test <- anes[indexes,]
+dim(anes2)
+partition <- sample(1:nrow(anes2), size=0.5*nrow(anes2))
+test <- anes2[partition,]
 dim(test) 
-train <- anes[-indexes,]
+train <- anes2[-partition,]
 dim(train)
 
-# statistical models
+# Statistical Models
 # First Model: Regress Obama Thermometer on Gender
-# Use variable gender_respondent_x for gender, as this has the gender for both
-# FTF and web modes
 # 1 is male and 2 is female
-# There does not appear to be any missing observations
-str(anes$gender_respondent_x)
-length(anes$gender_respondent_x)
-class(anes$gender_respondent_x)
-unique(anes$gender_respondent_x)
-# It is currently a factor, so it must be made numeric
-gender_num <- as.numeric(train$gender_respondent_x)
-# Regress against Obama Thermometer variable
-gen_model <- lm(ft_dpc ~ gender_num, train)
+# Regress Obama Thermometer against gender
+gen_model <- lm(Obama ~ Gender, train)
+summary(gen_model)
 
 # Second Model:  Obama Thermometer on education
-# use dem_edu, which has 18 unique outcomes
-# While the codebook doesn't specify every outcome, it can be assumed from
-# the information they do give that a higher value is more education
-# change from factor to nuumeric
-edu_num <- as.numeric(train$dem_edu)
-# look at unique values: they are numbers 1-19, but there is no 2
-table(edu_num)
+# Higher education numbers means higher level of education, from first grade to 
+# Doctorate
 # regress Obama thermometer on education
-edu_model <- lm(ft_dpc ~ edu_num, train)
+edu_model <- lm(Obama ~ Education, train)
+summary(edu_model)
 
-# Third model: Obama thermometer on vice president thermometer (Biden)
+# Third model: Obama thermometer on Republican opinions
+# Obama Thermometer regressed on opinions on Romney, George W Bush, and 
+# Republican Party
 table(train$ft_dvpc)
 # Has the same possible values as 
 
